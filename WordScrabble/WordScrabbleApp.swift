c7fc110eff3ee1a7c5fdb2fdcf12147761bd04df
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-02-02 15:06:29 +0530

    Initial Commit

---
+++ WordScrabble/WordScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrabbleApp.swift
+//  WordScrabble
+//
+//  Created by Dhurv Dugar on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
